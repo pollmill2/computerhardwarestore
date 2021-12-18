e2f139769db851a2e363b07142c8fd8bbad7427c
fhdav <fhdavidqqq@gmail.com>
2021-12-18 13:26:50 +0300

    Add UnitTestFixtute, Update ProductTests

---
+++ ComputerHardwareStore.Tests/UnitTests/Base/UnitTestFixture.cs
@@ -0,0 +1,69 @@
+﻿using ComputerHardwareStore.Entities;
+using ComputerHardwareStore.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ComputerHardwareStore.Tests.UnitTests.Base
+{
+    public class UnitTestFixture : IDisposable
+    {
+        protected DbContextOptions<ApplicationDbContext> Option;
+        protected ApplicationDbContext Context;
+
+        public UnitTestFixture()
+        {
+
+            Option = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("TestMemoryDb").Options;
+
+            Context = new ApplicationDbContext(Option);
+
+            var order = new Order
+            {
+                Address = "Some address",
+                Phone = "+375291111111",
+                FName = "Name",
+                LName = "Name",
+                CardItems =
+                    new List<ShoppingCartItem>
+                    {
+                        new ShoppingCartItem
+                        {
+                            Product = new Product {
+                                Id = 3,
+                                ProductName = "Name",
+                                Date = DateTime.Now,
+                                Image = "",
+                                Rating = 0,
+                                Price = 10,
+                                Specification = "8Gb RAM",
+                                CategoryId = 1,
+                            },
+                        }
+                    }
+            };
+
+            Context.Add(order);
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Task.Run(() =>
+            {
+                return Task.FromResult(true);
+            });
+        }
+    }
+}
---
+++ ComputerHardwareStore.Tests/UnitTests/Unit/ProductTests.cs
@@ -0,0 +1,87 @@
+﻿using ComputerHardwareStore.Controllers;
+using ComputerHardwareStore.Tests.UnitTests.Base;
+using ComputerHardwareStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ComputerHardwareStore.Tests.UnitTests.Unit
+{
+    public class ProductTests : UnitTestFixture
+    {
+        public ProductTests()
+        {
+
+        }
+
+        [Fact]
+        public async Task AddProdcutPost_ReturnsAViewResult_WhenModelStateIsInvalid()
+        {
+            var controller = new ProductController(Context);
+            controller.ModelState.AddModelError("ProductName", "Required");
+
+            var model = new AddProductViewModel()
+            {
+
+            };
+
+            // Act
+            var result = await controller.AddProduct(model);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            //Assert.IsType<SerializableError>(viewResult.Value);
+        }
+
+        [Fact]
+        public async Task AddProdcutPost_ReturnsARedirectToActionResult_WhenModelStateIsValid()
+        {
+            var controller = new ProductController(Context);
+
+            var model = new AddProductViewModel()
+            {
+
+            };
+
+            // Act
+            var result = await controller.AddProduct(model);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        [Theory]
+        [InlineData(4, true)]
+        public async Task DeleteProdcutPost_ReturnsANotFoundResult(int id, bool ready)
+        {
+            var controller = new ProductController(Context);
+
+            // Act
+            var result = await controller.DeleteProduct(id, ready);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Theory]
+        [InlineData(3, false, true)]
+        [InlineData(3, true, false)]
+        public async Task DeleteProdcutPost_ReturnsARedirectToActionResult(int id, bool ready, bool modelIsValid)
+        {
+            var controller = new ProductController(Context);
+
+            if (!modelIsValid)
+            {
+                controller.ModelState.AddModelError("ProductName", "Required");
+            }
+
+            // Act
+            var result = await controller.DeleteProduct(id, ready);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+    }
+}
