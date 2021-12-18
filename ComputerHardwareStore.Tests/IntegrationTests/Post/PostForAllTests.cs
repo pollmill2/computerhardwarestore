0ad8852bc51fe124054967d9c3196f975bd6d579
fhdav <fhdavidqqq@gmail.com>
2021-12-18 14:04:08 +0300

    Delete OrderPostTests, Add PostForAllTests

---
+++ ComputerHardwareStore.Tests/IntegrationTests/Post/PostForAllTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ComputerHardwareStore.Controllers;
+using ComputerHardwareStore.Entities;
+using ComputerHardwareStore.Models;
+using ComputerHardwareStore.Tests.IntegrationTests.Base;
+using ComputerHardwareStore.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace ComputerHardwareStore.Tests.IntegrationTests.Post
+{
+    public class PostForAllTests : IntegrationTestFixture
+    {
+        [Theory]
+        [InlineData("/Product/AddProduct")]
+        [InlineData("/Product/DeleteProduct")]
+        [InlineData("/Product/EditProduct")]
+        [InlineData("/Order/AddOrder")]
+        [InlineData("/ShoppingCart/Index")]
+        public async Task Post_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = Factory.CreateClient();
+
+            // Act
+            var model = new OrderViewModel()
+            {
+
+            };
+            var stringContent = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, stringContent);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
