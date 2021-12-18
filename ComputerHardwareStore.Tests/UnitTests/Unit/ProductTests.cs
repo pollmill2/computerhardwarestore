ebe19b7f29d609adaf172578c5ffa16884b15b32
fhdav <fhdavidqqq@gmail.com>
2021-12-18 22:11:01 +0300

    Update unitTests

--- ComputerHardwareStore.Tests/UnitTests/Unit/ProductTests.cs
+++ ComputerHardwareStore.Tests/UnitTests/Unit/ProductTests.cs
@@ -1,7 +1,9 @@
 ﻿using ComputerHardwareStore.Controllers;
+using ComputerHardwareStore.Entities;
 using ComputerHardwareStore.Tests.UnitTests.Base;
 using ComputerHardwareStore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,9 +17,21 @@ namespace ComputerHardwareStore.Tests.UnitTests.Unit
         }
 
         [Fact]
+        public void AddProdcutGet_ReturnsAViewResult()
+        {
+            var controller = new ProductController(context);
+
+            // Act
+            var result = controller.AddProduct();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
         public async Task AddProdcutPost_ReturnsAViewResult_WhenModelStateIsInvalid()
         {
-            var controller = new ProductController(Context);
+            var controller = new ProductController(context);
             controller.ModelState.AddModelError("ProductName", "Required");
 
             var model = new AddProductViewModel()
@@ -30,13 +44,12 @@ namespace ComputerHardwareStore.Tests.UnitTests.Unit
 
             // Assert
             Assert.IsType<ViewResult>(result);
-            //Assert.IsType<SerializableError>(viewResult.Value);
         }
 
         [Fact]
         public async Task AddProdcutPost_ReturnsARedirectToActionResult_WhenModelStateIsValid()
         {
-            var controller = new ProductController(Context);
+            var controller = new ProductController(context);
 
             var model = new AddProductViewModel()
             {
@@ -51,11 +64,35 @@ namespace ComputerHardwareStore.Tests.UnitTests.Unit
             Assert.Equal("Index", redirectToActionResult.ActionName);
         }
 
+        [Fact]
+        public void DeleteProdcutGet_ReturnsAViewResult_WhenProductIsExist()
+        {
+            var controller = new ProductController(context);
+
+            // Act
+            var result = controller.DeleteProduct(3);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void DeleteProdcutGet_ReturnsAViewResult_WhenProductIsNotExist()
+        {
+            var controller = new ProductController(context);
+
+            // Act
+            var result = controller.DeleteProduct(4);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
         [Theory]
         [InlineData(4)]
         public async Task DeleteProdcutPost_ReturnsANotFoundResult(int id)
         {
-            var controller = new ProductController(Context);
+            var controller = new ProductController(context);
 
             // Act
             var result = await controller.DeleteProduct(id, true);
@@ -70,7 +107,7 @@ namespace ComputerHardwareStore.Tests.UnitTests.Unit
         [InlineData(2, true, true)]
         public async Task DeleteProdcutPost_ReturnsARedirectToActionResult(int id, bool ready, bool modelIsValid)
         {
-            var controller = new ProductController(Context);
+            var controller = new ProductController(context);
 
             if (!modelIsValid)
             {
@@ -89,13 +126,73 @@ namespace ComputerHardwareStore.Tests.UnitTests.Unit
         [InlineData(3)]
         public async Task DeleteProdcutPost_ReturnsAViewResult(int id)
         {
-            var controller = new ProductController(Context);
+            var controller = new ProductController(context);
 
             // Act
             var result = await controller.DeleteProduct(id, true);
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void EditProdcutGet_ReturnsAViewResult_WhenProductIsExist()
+        {
+            var controller = new ProductController(context);
+
+            // Act
+            var result = controller.EditProduct(3);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void EditProdcutGet_ReturnsAViewResult_WhenProductIsNotExist()
+        {
+            var controller = new ProductController(context);
+
+            // Act
+            var result = controller.EditProduct(4);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task EditProdcutPost_ReturnsAViewResult_WhenModelStateIsInvalid()
+        {
+            var controller = new ProductController(context);
+            controller.ModelState.AddModelError("ProductName", "Required");
+
+            var model = new AddProductViewModel()
+            {
+
+            };
+
+            // Act
+            var result = await controller.EditProduct(2, model);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task EditProdcutPost_ReturnsARedirectToActionResult_WhenModelStateIsValid()
+        {
+            var controller = new ProductController(context);
+
+            var model = new AddProductViewModel()
+            {
+
+            };
+
+            // Act
+            var result = await controller.EditProduct(2, model);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
         }
     }
 }
---
+++ ComputerHardwareStore.Tests/UnitTests/Unit/ShoppingCartEntityTests.cs
@@ -0,0 +1,45 @@
+﻿using ComputerHardwareStore.Controllers;
+using ComputerHardwareStore.Entities;
+using ComputerHardwareStore.Tests.UnitTests.Base;
+using ComputerHardwareStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ComputerHardwareStore.Tests.UnitTests.Unit
+{
+    public class ShoppingCartEntityTests : UnitTestFixture
+    {
+        public ShoppingCartEntityTests() 
+        {
+                
+        }
+
+        [Fact]
+        public void GetShoppingCartItems_ReturnsList()
+        {
+            var shoppingCart = new ShoppingCart(context);
+
+            // Act
+            var result = shoppingCart.GetShoppingCartItems();
+
+            // Assert
+            Assert.IsType<List<ShoppingCartItem>>(result);
+        }
+
+        [Fact]
+        public void GetShoppingCartTotal_ReturnsList()
+        {
+            var shoppingCart = new ShoppingCart(context);
+
+            // Act
+            var result = shoppingCart.GetShoppingCartTotal();
+
+            double excepted = 10;
+
+            // Assert
+            Assert.Equal(excepted, result);
+        }
+    }
+}
